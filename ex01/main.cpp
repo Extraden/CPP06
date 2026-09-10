f54ef84522cbbe82612352948e9e2f3b64600bc7
dsemenov <dsemenov@student.42lyon.fr>
2026-09-10 19:46:02 +0200

    feat: add output after checking for equality

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -4,16 +4,23 @@
 
 int	main()
 {
-  uintptr_t ptr;
-  Data *data = new Data;
+  uintptr_t ptr = 0;
+  Data data;
+  data.value = 42;
 
-  std::cout << data << "\n";
+  std::cout << "Initial address: " << &data << "\n"
+            << "Initial value: " << data.value << "\n";
 
-  ptr = Serializer::serialize(data);
+  ptr = Serializer::serialize(&data);
 
   Data  *result = Serializer::deserialize(ptr);
 
-  std::cout << result << "\n";
-  delete data;
+  std::cout << "Result address: " << result << "\n"
+            << "Result value: " << result->value << "\n";
+
+  if (&data == result)
+    std::cout << "Addresses are equal\n";
+  else
+   std::cout << "Addresses are not equal\n";
 	return 0;
 }
